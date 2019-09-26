34c3b363a2386a62754dedf454f4cc9d9e5393e0
Tanner York <tannerwyork@gmail.com>
2019-09-26 15:43:13 -0700

    [Add] Allow users to add new playlist to playlists database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -6,17 +6,26 @@ db = client.Playlister
 playlists = db.playlists
 app = Flask(__name__)
 
-# OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
-
 @app.route('/')
 def playlists_index():
     '''Show all the playlist the the user'''
     return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists/new')
+def playlist_new():
+    '''Show form for creating a new playlist'''
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    '''Submit new playlist from the user'''
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
