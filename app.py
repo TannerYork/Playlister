9907249620ff2932f9aecb4abee18b72e367bea8
Tanner York <tannerwyork@gmail.com>
2019-09-26 17:15:22 -0700

    [Add] Create the edit playlist feature

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlist_new():
     '''Show form for creating a new playlist'''
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title="New Playlist")
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -34,6 +34,25 @@ def playlists_show(playlist_id):
     '''Show a single playlist'''
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    '''Show the edit form for the user to edit a specific playlist'''
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title="Edit Playlist")
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
